3f0e8d545c5b45af9f7124574b1893e185d1b1c6
silvaPool <silva.friends22@gmail.com>
2024-02-26 14:04:26 -0300

    React-Cookie

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+import Cookies from 'js-cookie';
+
+function App() {
+
+  const SetCookie = () => {
+    Cookies.set("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9", {
+      expires: 7,
+    });
+  };
+
+  const GetCookie = () => {
+    alert(Cookies.get("token"));
+  };
+
+  const RemoveCookie = () => {
+    Cookies.remove("token");
+  }
+  
+
+  return (
+    <div style={{textAlign: "center"}}>
+      <h6>1. Click on Set Cookie to set data in cookies</h6>
+      <h6>2. Click on Get Cookie to display the data</h6>
+      <h6>2. Click on Remove Cookie to remove data from cookies</h6>
+      <button onClick={SetCookie}>Set Cookie</button>
+      <button onClick={GetCookie}>Get Cookie</button>
+      <button onClick={RemoveCookie}>Remove Cookie</button>
+    </div>
+  )
+}
+
+export default App
